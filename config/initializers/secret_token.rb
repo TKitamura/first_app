bc1852af95b4168e631d653020c1c6796a97cb9f
EC2 Default User <ec2-user@ip-10-146-11-253.ap-northeast-1.compute.internal>
2012-02-14 21:11:21 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '7eb7ec798045e37c34ab903287436b7df1cdf2d1ce77b4b969252e9b97ea1361914bfab34d6ad9fb3b868265c96fbea909b78b59bddc2718835d46d8369d83eb'
